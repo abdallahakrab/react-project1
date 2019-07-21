3b50fe49288b5a5442c63571b860258cb80642ec
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-07-21 19:22:19 +0200

    added unorded list

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,19 +3,33 @@
 console.log('App.js is running!');
 
 var template = React.createElement(
-  'div',
-  null,
-  '       ',
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Indecision App!'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'Welcome'
-  )
+    '                                                        ',
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Welcome'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'First Item'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Second Item'
+        )
+    )
 );
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- indecision-app/public/src/app.js
+++ indecision-app/public/src/app.js
@@ -1,9 +1,14 @@
 console.log('App.js is running!'); 
  
-var template = <div>       {/* adjacent elements without root element(div) will give error*/}  
-<h1>Indecision App!</h1>
-<p>Welcome</p>
-</div>;
+var template = ( 
+<div>                                                        {/* adjacent elements without root element(div) will give error*/}  
+    <h1>Indecision App!</h1>
+    <p>Welcome</p>
+    <ul>
+        <li>First Item</li>
+        <li>Second Item</li>
+    </ul>
+</div>);
 var appRoot = document.getElementById('app');
 ReactDOM.render(template,appRoot);
 
