d8b30a27413bf28cc885f81154d1a9612208a6d1
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-08 19:12:37 +0200

    configuring webpack

---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+//cofinguration details for webpack build
+module.exports = { 
+  //where webpack should start (The main file)  
+  entry: './src/app.js', 
+   //of the output file , must be absolute (complete path) but there is variable which will make it easier:
+    // __dirname path to Indecision-app folder and using node function to concatenate paths ,
+    // we wont use string concatenation due to edge cases
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js' // can be any name but this is the most common
+  }
+};
+
+console.log("print",path.join(__dirname, 'public'));
