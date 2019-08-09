976da536143d507d63ed3739efe40936b636e2e8
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-09 17:15:23 +0200

    webpack dev-server setup

--- indecision-app/src/components/Header.js
+++ indecision-app/src/components/Header.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Header = (props) => {
     return (
         <div>
-    
          <h1>{props.title}</h1>
          {props.subtitle && <h2>{props.subtitle}</h2>}
         </div>
