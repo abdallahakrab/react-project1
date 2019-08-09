2193e6385affcf10e2e990813fd01a0185953772
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-09 21:32:04 +0200

    trying out built in children prop for dynamic rendering

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,7 +3,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render( <IndecisionApp /> , document.getElementById('app'));
+//trying built-in children prop
+
+const Comp = (props) => {
+return (
+    <div>
+     <p>Header</p>
+     {props.children}
+     <p>Footer</p>
+
+    
+    </div>
+)
+};
+
+
+ReactDOM.render( <Comp>
+    <h1>test</h1>
+    </Comp> 
+    , document.getElementById('app'));
 
  
 
