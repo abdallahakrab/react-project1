b12abf2d64c896eb77c1b626b32507345e05d8f4
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-09 10:58:19 +0200

    exploring adding third library to the project

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,14 +1,4 @@
-import subtract, { square, add } from  './utils.js';  // ./ brings us to the current file directory 
+import validator from 'validator';
 
-import isSenior, {isAdult, canDrive} from './person.js';
+console.log(validator.isEmail('test@gmail.com'));
 
-console.log("from app.js");
-console.log(square(3));
-console.log(add(100,23));
-console.log(subtract(100,81));
-
-
-console.log(isAdult(22));
-console.log(canDrive(15));
-console.log(isSenior(63));
-console.log(isSenior(67));
