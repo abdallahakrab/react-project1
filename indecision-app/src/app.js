88b4ad4e0855999cc624eecb783bc31e6d8ac5e9
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-10 07:49:36 +0200

    setting up css with webpack , added style-loader and css-loader

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.css';
 
 
 
 ReactDOM.render( <IndecisionApp />, document.getElementById('app'));
-
+    
  
 
--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -15,7 +15,18 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
-  }]
+      },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+        
+      ]
+    }]
+    
+    
+
 },
 devtool: 'cheap-module--eval-source-map',
 
